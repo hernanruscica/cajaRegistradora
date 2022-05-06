226e08156c831284f4111aacfc98892325d9d23d
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-05-06 17:52:18 -0300

    Add files via upload
    funcionando bastante bien. genera bien el vuelto, lo descuenta del efectivo en caja y si no lo consigue no lo descuenta. faltaria ver el tema de retornar todo con comas. y mostrar por html

--- CajaRegistradora.js
+++ CajaRegistradora.js
@@ -21,16 +21,17 @@ ordenados de mayor a menor, con el valor de "cambio".
 */   
 
 //efectivo en caja con comas...
+
 let ejemploEfectivoEnCaja = [
-    ["PENNY", 0.02],
-    ["NICKEL", 0.10],
-    ["DIME", 0.2],
+    ["PENNY", 0],
+    ["NICKEL", 0],
+    ["DIME", 0],
     ["QUARTER", 0.50],
-    ["ONE", 2],
-    ["FIVE", 10],
-    ["TEN", 20],
-    ["TWENTY", 40],
-    ["ONE HUNDRED", 200]
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
   ]
 
   //divisas con comas...
@@ -51,12 +52,13 @@ function sacarComas(efectivoArray){
     temp.forEach(divisa => divisa[1] = Math.trunc(parseFloat(divisa[1] * 100)));
     return temp
 }
-/*
+
 function ponerComas(efectivoArray){
     const temp = [...efectivoArray];
-    temp.forEach(divisa => divisa[1] = Math)
+    temp.forEach(divisa => divisa[1] = parseFloat(divisa[1] / 100));
+    return temp;
 }
-*/
+/**/
 const divisasSinComas = sacarComas(divisas);
 /*
 const ejemploEfectivoEnCajaSinComas = sacarComas(ejemploEfectivoEnCaja)
@@ -87,17 +89,21 @@ function calcularCambioDebido(pago, precio){
     La funcion arma el vuelto (cambio a dar), en base a lo que hay en la caja.
     si no alcanza, retorna "false", puede ser por falta de divisas o total de dinero
 */
-function sacarCambio(cambio, divisas, efectivo){      
+function sacarCambio(cambio, divisas, efectivoPasado){      
     const longitudDivisas = divisas.length - 1;    
     const cambioAdar = [];   
-
+    const efectivo = [...efectivoPasado];
+    const efectivoSinTocar = [...efectivoPasado];
+    //console.log(efectivoPasado);
     const buscarLaDivisa = (nombreDivisa, efectivo) => {      
         //console.log(nombreDivisa);
         //console.log(efectivo);
         let encontrada = false;
         let indiceEncontrada = 0;
         efectivo.forEach((efectivo, indice) => {
-            if (efectivo[0] === nombreDivisa){
+            let nombreDivisaActual = efectivo[0];
+
+            if ( nombreDivisaActual === nombreDivisa){
                 encontrada = true;
                 indiceEncontrada = indice;
             }
@@ -105,22 +111,35 @@ function sacarCambio(cambio, divisas, efectivo){
         //console.log(encontrada, indiceEncontrada);
         return [encontrada, indiceEncontrada];
     }
-    for (i = longitudDivisas; i >= 0; i--){        
-        while (efectivo[i][1] > 0 && cambio > 0 && cambio >= divisas[i][1]){
-            efectivo[i][1] -= divisas[i][1];
-            cambio -= divisas[i][1]; 
+    //console.log(buscarLaDivisa("ONE", [["ONE", 2],["PENNY", 12]])); //retorna [true, 0]
 
-            let resultadoBusqueda = buscarLaDivisa(divisas[i][0], cambioAdar);
+    for (i = longitudDivisas; i >= 0; i--){     
+        let valorEfectivoActual = efectivo[i][1];
+        let valorDivisaActual = divisas[i][1];
+        let nombreDivisaActual = divisas[i][0];
+
+        while (valorEfectivoActual > 0 && cambio > 0 && cambio >= valorDivisaActual){
+
+            valorEfectivoActual -= valorDivisaActual;
+            cambio -= valorDivisaActual; 
+
+            let resultadoBusqueda = buscarLaDivisa(nombreDivisaActual, cambioAdar);
             let indiceResultadoBusqueda = resultadoBusqueda[1];
             let estaLaDivisa = resultadoBusqueda[0]; 
 
-            if (!estaLaDivisa) cambioAdar.push([divisas[i][0], divisas[i][1]]);             
-            else cambioAdar[indiceResultadoBusqueda][1] += divisas[i][1]; 
+            if (!estaLaDivisa) cambioAdar.push([nombreDivisaActual, valorDivisaActual]);             
+            else cambioAdar[indiceResultadoBusqueda][1] += valorDivisaActual; 
         }
     }
-    return (cambio == 0.00)?[cambioAdar, true]:[cambioAdar, false]; 
+
+    //return (cambio == 0.00)?[cambioAdar, true, efectivo]:[cambioAdar, false, efectivoSinTocar]; 
+    if (cambio == 0.00) {
+        return [cambioAdar, true]
+    } else {
+        return [cambioAdar, false]
+    }
 }
-//console.log(sacarCambio(3, divisasSinComas, ejemploEfectivoEnCajaSinComas));
+//console.log(sacarCambio(9,5, divisasSinComas, sacarComas(ejemploEfectivoEnCaja)));
 
 //actualiza la cantidad de efectivo en la caja, despues de haber podido dar un vuelto.
 //la segunda variable es lo que se tiene que sacar de la caja. Retorna la nueva caja
@@ -137,55 +156,49 @@ function actualizarEfectivoEnCaja(efectivoEnCajaAnterior, actualizacionCaja){
 }
 /*
 console.log(actualizarEfectivoEnCaja([["PENNY", 1], ["NICKEL", 205], ["DIME", 31], ["QUARTER", 425], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]
-, [["TEN", 1000], ["FIVE", 500], ["ONE", 200], ["QUARTER", 275], ["DIME", 20]]));
+, [["TEN", 10], ["FIVE", 5], ["ONE", 20], ["QUARTER", 275], ["DIME", 20]]));
 */
 
 /***************************** HASTA ACA REVISADO **********************************************************/
 
 //Funcion principal, recibe los valores con decimales, dentro se los quita con la funcion sacarComas();
-function revisarCajaRegistradora(precioCompraPasado, pagoEfectuadoPasado, efectivoEnCaja){    
+function revisarCajaRegistradora(precioCompraPasado, pagoEfectuadoPasado, efectivoEnCajaPasado){    
     
     let precioCompra = Math.trunc(parseFloat(precioCompraPasado*100));
     let pagoEfectuado = Math.trunc(parseFloat(pagoEfectuadoPasado*100));
-    efectivoEnCaja = sacarComas(efectivoEnCaja);     
+    let efectivoEnCaja = sacarComas(efectivoEnCajaPasado);     
     
-    if (precioCompra > pagoEfectuado) return {estado: "PAGO_INSUFICIENTE", change: []};
-
+    if (precioCompra > pagoEfectuado) return {estado: "PAGO_INSUFICIENTE", cambio: [], estadoCaja: efectivoEnCaja};
+    
     let totalEfectivoEnCaja = calcularTotalEfectivoEnCaja(efectivoEnCaja);
-    let cambioDebido = calcularCambioDebido(pagoEfectuado,  precioCompra);
+    let cambioDebido = calcularCambioDebido(pagoEfectuado,  precioCompra);    
 
-    console.log(`compra: ${precioCompra},su pago: ${pagoEfectuado}`);
-    console.log(`Cambio debido: ${cambioDebido}`);     
-    console.log(efectivoEnCaja);
-    console.log(`total efectivo: ${totalEfectivoEnCaja}`);   
-
-    let resultadoSacarCambio = sacarCambio(cambioDebido, divisasSinComas, efectivoEnCaja);
-    let tengoCambioExacto = resultadoSacarCambio[1];
-    let cambioExacto = resultadoSacarCambio[0];
-    let efectivoEnCajaActualizado = [];
-
-    if (tengoCambioExacto) {
-        console.log(cambioExacto);    
-        //efectivoEnCajaActualizado = actualizarEfectivoEnCaja(efectivoEnCaja, cambioExacto);
-    }
-    else console.log("no tengo cambio");
-
-    /*determino el estado a retornar*/
-    if (totalEfectivoEnCaja < cambioDebido) return {estado: "SALDOS_INSUFICIENTES", cambio: [], estadoCaja: efectivoEnCaja};
-    if (totalEfectivoEnCaja === cambioDebido) return {estado: "CAJA_CERRADA", cambio: cambioExacto, estadoCaja: efectivoEnCaja};
-    if (!tengoCambioExacto) return {estado: "SALDOS_INSUFICIENTES_CAMBIO", cambio: [], estadoCaja: efectivoEnCaja};
+    //console.log(`total cambio en caja: ${totalEfectivoEnCaja} cambio debido: ${cambioDebido}`);
     
-    //esta ultima actualizacion del estado de la caja registradora no lo piden en el ejercicio.    
-    //let efectivoEnCajaActualizado = actualizarEfectivoEnCaja(efectivoEnCaja, tengoCambioExacto[0]);
+    let resultadoSacarCambio = sacarCambio(cambioDebido, divisasSinComas, efectivoEnCaja);    
+    let tengoCambioExacto = resultadoSacarCambio[1];
+    let cambioExacto = resultadoSacarCambio[0]; 
+
+    /*determino el estado a retornar  - problema con SALDOS_INSUFICIENTES_CAMBIO sigue descontando aunque no pueda dar el cambio por falta de billetes*/
+    if (totalEfectivoEnCaja < cambioDebido) return {estado: "SALDOS_INSUFICIENTES", cambio: [], estadoCaja: efectivoEnCaja};    
+    if (totalEfectivoEnCaja === cambioDebido) return {estado: "CAJA_CERRADA", cambio: ponerComas(cambioExacto), estadoCaja: actualizarEfectivoEnCaja(efectivoEnCaja, cambioExacto)};
+    if (!tengoCambioExacto) {          
+        return {estado: "SALDOS_INSUFICIENTES_CAMBIO", cambio: [], estadoCaja: ponerComas(efectivoEnCaja)};
+        }
+    //esta ultima actualizacion del estado de la caja registradora no lo piden en el ejercicio.        
     return {
         estado: 'OPEN',
-        cambio: cambioExacto,
-        estadoCaja: efectivoEnCaja
+        cambio: cambioExacto, //si le pongo el ponerComas a cambioExacto, se jode el estadoCaja
+        estadoCaja: ponerComas(actualizarEfectivoEnCaja(efectivoEnCaja, cambioExacto))
     }
-    /**/
+    
 }
-//poner con comas
-console.log(revisarCajaRegistradora(0.1, 0.4, ejemploEfectivoEnCaja));
+console.log(revisarCajaRegistradora(1, 1.25, ejemploEfectivoEnCaja));
+
+
+
+/***********************************hasta aca controlado **********************************/
+
 
 /* TODAS LAS PRUEBAS DE FREECODECAMP PASADAS !! */
 //console.log(revisarCajaRegistradora(0.5, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
@@ -199,14 +212,17 @@ console.log(revisarCajaRegistradora(0.1, 0.4, ejemploEfectivoEnCaja));
 function darVuelto(compraValorId, pagoValorId){
     const $compraValorId = document.getElementById(compraValorId);
     const $pagoValorId = document.getElementById(pagoValorId);  
+    const efectivoEnCaja = [...ejemploEfectivoEnCaja];
 
     let valorCompra = parseFloat($compraValorId.value);
     let valorPago = parseFloat($pagoValorId.value);
-    const resultadoRevisarCajaRegistradora = revisarCajaRegistradora(valorCompra, valorPago, ejemploEfectivoEnCaja);      
+    const resultadoRevisarCajaRegistradora = revisarCajaRegistradora(valorCompra, valorPago, efectivoEnCaja);      
 
+    //console.clear();
     console.log(valorCompra, typeof(valorCompra));
     console.log(valorPago, typeof(valorPago));
     console.log(resultadoRevisarCajaRegistradora);
+    ejemploEfectivoEnCaja = resultadoRevisarCajaRegistradora.estadoCaja;
 
     //console.log(`Total efectivo en caja: ${acumulador}`);
     
